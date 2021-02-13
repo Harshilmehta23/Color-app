48cb15a0c6d8b67e026f1f2fbec1a50f01cae4ac
Harshilmehta23 <mharshil60@gmail.com>
2021-02-13 15:33:31 +0530

    Add delete icon to mini palette

--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -40,6 +40,19 @@ const styles = {
     postition: 'relative',
     marginBottom: '-3.5px',
   },
+  delete: {},
+  deleteIcon: {
+    color: 'white',
+    backgroundColor: '#eb3d30',
+    width: '20px',
+    height: '20px',
+    position: 'absolute',
+    right: '0',
+    top: '0',
+    padding: '10px',
+    zIndex: '10',
+    opacity: '0',
+  },
 };
 
 export default styles;
